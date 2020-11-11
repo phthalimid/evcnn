f5f0ffec102a0aba7dda2b8cea101568a0ba460f
phthalimid <th.nann@gmail.com>
2020-11-11 12:33:43 +1100

    Rough outline of the project

--- evcnn.py
+++ evcnn.py
@@ -6,8 +6,39 @@ Created on Tue Nov 10 15:47:31 2020
 @author: tn438
 """
 
+import os
+# Turn off TensorFlow warning messages in program output
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
 import logging
+
+import tensorflow as tf
+
+from model import EVCNN
+
+
+@tf.function
+def train_step(images, labels):
+    with tf.GradientTape() as tape:
+        # training=True is only needed if there are layers with different
+        # behavior during training versus inference (e.g. Dropout).
+        predictions = model(images, training=True)
+        loss = loss_object(labels, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+    train_loss(loss)
+    train_accuracy(labels, predictions)
+
+@tf.function
+def test_step(images, labels):
+    # training=False is only needed if there are layers with different
+    # behavior during training versus inference (e.g. Dropout).
+    predictions = model(images, training=False)
+    t_loss = loss_object(labels, predictions)
+
+    test_loss(t_loss)
+    test_accuracy(labels, predictions)
 
 
 def main():
@@ -15,12 +46,42 @@ def main():
     
     # Read in data
     
-    # ----> TRAIN
+    # Instantiate our neural network model from the predefined class. Also define the loss function and optimizer.
+    model = EVCNN()
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    optimizer = tf.keras.optimizers.Adam()
+
+    # Define the metrics for loss and accuracy.
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+    train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+    test_loss = tf.keras.metrics.Mean(name='test_loss')
+    test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
     
+    # Run and iterate model over epochs
+    EPOCHS = 5
+    for epoch in range(EPOCHS):
+
+        # Reset the metrics at the start of the next epoch
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+        test_loss.reset_states()
+        test_accuracy.reset_states()
+
+        # Train then test the model
+        for images, labels in train_ds:
+            train_step(images, labels)
+        for test_images, test_labels in test_ds:
+            test_step(test_images, test_labels)
+
+        # Print results
+        template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
+        print(template.format(epoch + 1,
+                            train_loss.result(),
+                            train_accuracy.result() * 100,
+                            test_loss.result(),
+                            test_accuracy.result() * 100))
+        print("time elapsed: {:.2f}s".format(time.time() - start_time))    
     
-    
-    
-    pass
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -12,17 +12,46 @@ https://www.sciencedirect.com/science/article/pii/S0003267019307342?via%3Dihub
 
 import tensorflow as tf
 from tensorflow.keras import Model
-from tensorflow.keras.layers import Dense, Flatten, Conv1D, MaxPool2D, BatchNormalization, Dropout
+from tensorflow.keras.layers import Dense, Flatten, Conv1D, MaxPool1D, BatchNormalization, Dropout
 
 
-
+FILTERS = 32
+KERNEL_SIZE = 3
+STRIDE = 3
+POOL_SIZE = 2
+DENSE_SIZE = 128
 
 class EVCNN(Model):
     
     def __init__(self):
         super(EVCNN, self).__init__()
     
-        self.conv = Conv1D()
-    
-    pass
+        self.conv1 = Conv1D(FILTERS, KERNEL_SIZE, strides=STRIDE, activation='relu')
+        self.conv2 = Conv1D(FILTERS, KERNEL_SIZE, strides=STRIDE, activation='relu')
+        self.conv3 = Conv1D(FILTERS, KERNEL_SIZE, strides=STRIDE, activation='relu')
+        self.pool = MaxPool1D(POOL_SIZE)
+        self.batchnorm = BatchNormalization()
+        self.dropout = Dropout(rate=0.4)      
+        self.flatten = Flatten()
+        self.dense = Dense(DENSE_SIZE, activation='relu')
+        self.softmax = Dense(10, activation='softmax')
 
+        
+    def call(self, x, train=False):
+
+        x = self.conv1(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        x = self.pool(x)
+        x = self.conv3(x)
+        x = self.batchnorm(x)
+        
+        if train:
+            x = self.dropout(x, training=train)
+
+        x = self.flatten(x)
+        x = self.dense(x)
+        x = self.softmax(x)
+
+        return x        
+    
